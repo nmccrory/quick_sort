43bf818dd28138ca9bed0ff0f80c4560b02d8d70
Nick McCrory <n.mccrory@icloud.com>
2017-04-25 16:21:46 -0700

    Got rid of the n parameter in the partition function. It was unneccessary after changing the pivot element to be the first element of the list.

--- quick_sort.ml
+++ quick_sort.ml
@@ -1,22 +1,19 @@
 (* Quick sort algorithm *)
-let rec partition n p l = 
-	match (n, l) with
-	| (0, _)-> ([], [])
-	| (_, [])-> ([], [])
-	| (_, h::t) when (h <= p)-> let (left, right) = partition (n-1) p t in
-									(h::left, right)
-	| (_, h::t)-> let (left, right) = partition (n-1) p t in 
+let rec partition p l = 
+	match l with
+	| []-> ([], [])
+	| h::t when (h <= p)-> let (left, right) = partition p t in
+									(h::left, right) 
+	| h::t-> let (left, right) = partition p t in 
 									(left, h::right)
 
 let rec quick_sort l = 
 	match l with
 	| []-> []
-	| h::t -> let last = (List.length l)-1 in
-				let p = List.nth l last in
-				let (left, right) = partition last p l in
+	| h::t -> let (left, right) = partition h t in
 				let sortedLeft = quick_sort left in
 				let sortedRight = quick_sort right in
-				sortedLeft @ p::sortedRight
+				sortedLeft @ h::sortedRight
 
 let print_list l = 
 	List.iter (Printf.printf "%d\n") (quick_sort l);;
